716ea27c035af159ae205f3f4f0f8b2fffb158fd
monashamsolebad <shamsolebad.mona@gmail.com>
2019-09-02 21:39:54 -0700

    first commit

---
+++ PersonalityQuiz/QuestionData.swift
@@ -0,0 +1,9 @@
+//
+//  QuestionData.swift
+//  PersonalityQuiz
+//
+//  Created by Mona Shamsolebad on 2019-08-27.
+//  Copyright © 2019 Mona Shamsolebad. All rights reserved.
+//
+
+import Foundation
